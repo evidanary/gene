a076ef29b202b64cb72691adb180eddf1e853f64
yash-ranadive <yash.ranadive@gmail.com>
2012-04-08 21:53:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gene::Application.config.secret_token = 'ef5111b5bc3e93d1d2fd981910db184c34d7866cecbcb89a9888160f068590514a24694fe708d20d20da53567f3c66e8e123c34d8e35b5133a0d8a262fc014f9'
